96e3550545e264350a7698c13fdef670c8f62a5d
muhammertasci11 <muhammertasci11@users.noreply.github.com>
2026-06-13 14:21:27 +0300

    feat: Fix corrupted icon & add smart first-run shortcut creation

--- Dosyaktar/App.xaml.cs
+++ Dosyaktar/App.xaml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Windows;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace Dosyaktar
 {
@@ -8,6 +10,36 @@ namespace Dosyaktar
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            // Smart First-Run: Masaüstü Kısayolu Oluşturma
+            try
+            {
+                string exePath = Process.GetCurrentProcess().MainModule?.FileName;
+                string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string shortcutPath = Path.Combine(desktop, "Dosyaktar.lnk");
+
+                if (exePath != null && !File.Exists(shortcutPath))
+                {
+                    string vbsPath = Path.Combine(Path.GetTempPath(), "CreateDosyaktarShortcut.vbs");
+                    string vbsCode = $@"
+Set WshShell = CreateObject(""WScript.Shell"")
+Set Shortcut = WshShell.CreateShortcut(""{shortcutPath}"")
+Shortcut.TargetPath = ""{exePath}""
+Shortcut.WorkingDirectory = ""{Path.GetDirectoryName(exePath)}""
+Shortcut.Save
+";
+                    File.WriteAllText(vbsPath, vbsCode);
+                    var p = Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "wscript.exe",
+                        Arguments = $"\"{vbsPath}\"",
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    });
+                    p?.WaitForExit();
+                    if (File.Exists(vbsPath)) File.Delete(vbsPath);
+                }
+            }
+            catch { /* Sessizce devam et */ }
             AppDomain.CurrentDomain.UnhandledException += (s, args) =>
             {
                 var ex = args.ExceptionObject as Exception;
